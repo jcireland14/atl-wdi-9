a43da0f97314489f0ac6a416430bf91869c057cf
Jcireland14 <Jcireland14@gmail.com>
2017-02-28 21:41:46 -0500

    homework

--- unit_01/w01d04/student_labs/js-functions-lab/functions-lab-starter.js
+++ unit_01/w01d04/student_labs/js-functions-lab/functions-lab-starter.js
@@ -1,61 +1,96 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(2, 4) {
+  if (2 < 4) {
+    return 4;
+  }
+  else if (2 > 4) {
+    return 2;
+  }
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(5, 3, 4) {
+  if (5 > 3 && 5 > 4) {
+    return 5;
+  }
+  if (3 > 5 && 3> 4) {
+    return 3;
+  }
+  if (4 > 3 && 4 > 5) {
+    return 4;
+  }
 }
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(y) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  if (vowels.indexOf(c) !== -1) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+//And sometimes Y...
 
 // Question 4
-function sumArray() {
- 
+function sumArray(arrayUno) {
+  var sum = arrayDos[0];
+  for (var i =0; i < arrayUno.length; i++) {
+    sum += array1Uno[i];
+  }
+  return sum;
 }
 
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(arrayDos) {
+var product = arrayDos[0];
+for (var i = 1; i < arrayDos.length; i++) {
+  product *= arrayDos[i];
+}
+return product;
 }
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+  return args.length;
 }
 
 
 
 // Question 6
 var reverseString = function (){
-  
+  return this.split('')
+             .reverse()
+             .join('')
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (array1) {
+  longest = 0;
+  for (var i =0; i < array1.length; i++) {
+    if (array1[i].length > longest) {
+      longest = array1[i].length;
+
+    }
+  }
+  return longest;
 }
+
 
 
 // Question 8
-function filterLongWords () {
-  
-}
+function filterLongWords (array1, number1) {
+  array2 = [];
+  for(var i = 0; i < array1.length; i++) {
+    if(array1[i].length > num) {
+      array2.push(array1[i]);
+    }
+  }
+  return array2;
 
 
-// Bonus 1
-//??????
-
-
-// Bonus 2
-function charactersOccurencesCount() {
-  
-}
 
