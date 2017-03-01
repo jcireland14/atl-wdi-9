43ed578dc576cfe9b3c90c3de1d5f4cf38074b18
Jcireland14 <Jcireland14@gmail.com>
2017-02-28 21:56:28 -0500

    js-functions-lab/

--- unit_01/w01d04/student_labs/js-independent-practice/independent-practice-starter.js
+++ unit_01/w01d04/student_labs/js-independent-practice/independent-practice-starter.js
@@ -2,7 +2,22 @@
 // an array of strings. The function should return an array of numbers where each
 // number is the length of the corresponding string.
 
-// your code here
+//1. define the function
+//2. define an emptpy array
+//3. for each string in the original array
+//4. we are going to find the length
+//5. and push the length to the new array as a number
+//6. return the new array
+
+function lengths (arrayOfStrings){
+  var arrayOfNumbers = [];
+
+  for (var i=0; i< arrayOfStrings.length; i++) {
+    var lengthOfString = arrayOfStrings[i].length;
+    arrayOfNumbers.push(lengthOfString);
+  }
+  return arrayOfNumbers;
+}
 
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
@@ -11,10 +26,58 @@
 // The transmogrified result of three numbers is the product of the first two numbers,
 // raised to the power of the third number.
 
-// your code here
+//1. define function called transmogrifier
+//2. define three parameters called base1, base2, exponent
+//3. find the product of the first two numbers (base1 and base2)
+//4. raise that product to the power of exponent
+//5. return the transmogrofied number
+
+var transmogrifier = function (base1, base2, exponent) {
+var productOfBases = base1 * base2;
+return Math.pow(productOfBases, exponent);
+}
+
+
 
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
-// your code here
+//1. define function called "wordReverse"
+//2. define one pararmeter called stringToReverse
+//3. turn the string into array of words
+//4. create an empty variable to store reversed array
+//5. working backwards from the original array, place them into the empty array
+//6.return the reversed array as a string
+
+
+function wordReverse(stringToReverse){
+  var arrayOfWords = stringToReverse.split(' this java shit is for the birds');
+  var reversedWords = [];
+  reversedWords = arrayOfWords.reverse();
+  return reversedWords.join(' ');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
